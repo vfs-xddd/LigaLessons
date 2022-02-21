b47ef20ff69c7e7e3c294d06c64212d92a454e1b
vfs-xddd <vfs.xddd@gmail.com>
2022-02-21 12:22:36 +0300

    Test classes

---
+++ src/main/java/lesson_3/homework/Run.java
@@ -0,0 +1,9 @@
+package lesson_3.homework;
+
+public class Run {
+
+    public static void main(String[] args){
+        WebTile tile = new WebTile();
+        tile.open();
+    }
+}
---
+++ src/main/java/lesson_3/homework/WebTile.java
@@ -0,0 +1,9 @@
+package lesson_3.homework;
+
+public class WebTile extends Category{
+
+    @Override
+    public void open() {
+        System.out.println("worked!");
+    }
+}
