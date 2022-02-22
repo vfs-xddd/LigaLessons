dad05aa43cb07d753e657b22e252e5c69e427673
vfs-xddd <vfs.xddd@gmail.com>
2022-02-22 03:47:24 +0300

    WebContainer.java - add methods impl Element's, fields, complete this class Run.java - test this

--- src/main/java/lesson_3/homework/Run.java
+++ src/main/java/lesson_3/homework/Run.java
@@ -3,6 +3,7 @@ package lesson_3.homework;
 public class Run {
 
     public static void main(String[] args){
+        System.out.println("===Test WebTile===============================================");
         WebTile tile = new WebTile(100, 50, "Смартфоны", "Категории сматфонов", "http://smartphones");
         WebTile tile2 = new WebTile(500, 80);
         tile.status();
@@ -10,7 +11,19 @@ public class Run {
         tile.show_description();
         tile.on_click();
         tile.on_focus();
-
+        System.out.println("===Test WebContainer===============================================");
+        WebContainer container = new WebContainer(1000, 200, "Контейнер поиска", "Контейнер для строки поиска и мюню поиска рядом");
+        WebContainer container2 = new WebContainer(2000, 200, "Контейнер поиска2", "Контейнер для строки поиска и мюню поиска рядом");
+        container.add_obj(tile);
+        container.add_obj(tile2);
+        container2.add_obj(tile);
+        container.status();
+        System.out.println(container.type);
+        container.show_description();
+        container.on_click();
+        container.on_focus();
+        System.out.println("Объекты в контейнере: " + container.show_obj_list());
+        System.out.println("Объекты в контейнере: " + container2.show_obj_list());
 
     }
 }
--- src/main/java/lesson_3/homework/WebContainer.java
+++ src/main/java/lesson_3/homework/WebContainer.java
@@ -1,4 +1,59 @@
 package lesson_3.homework;
 
-public class WebContainer {
+import lesson_3.homework.abc.Category;
+import lesson_3.homework.interfaces.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebContainer extends Category implements Element {
+    protected String type = "WebContainer";
+    private final List<Category> obj_list = new ArrayList<>();
+
+    WebContainer(int width, int height) {
+        super(width, height, "No name", "Созданный объект Контейнер");
+    }
+
+    WebContainer(int width, int height, String name) {
+        super(width, height, name, "Созданный объект Контейнер");
+    }
+
+    WebContainer(int width, int height, String name, String iconDescription) {
+        super(width, height, name, iconDescription);
+    }
+
+    public void add_obj(Category obj) {
+        this.obj_list.add(obj);
+    }
+
+    public  List<Category> show_obj_list() {
+        return this.obj_list;
+    }
+
+    @Override
+    public void status() {
+        System.out.println("On Page");
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public void on_click() {
+    }
+
+    @Override
+    public void on_focus() {
+        System.out.println(this.type + "." + this.name + ": в фокусе, показываю описание - " + this.get_iconDescription());
+    }
+
+    @Override
+    public void if_Active() {
+    }
+
+    @Override
+    public void if_inActive() {
+    }
 }
